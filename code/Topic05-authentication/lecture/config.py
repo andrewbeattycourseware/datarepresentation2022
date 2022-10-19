ce8a232de0971fdf3f2989428b4704b159480497
Andrew Beatty <andrew.beatty@atu.ie>
2022-10-19 17:48:53 +0100

    topic05

---
+++ code/Topic05-authentication/lecture/config.py
@@ -0,0 +1,4 @@
+config = {
+    "htmltopdfkey":"eAs3mWn7R59gxZ6kSYysYdpTUsivY7Am74aSN36pOHdMMyTu3XDpdSOtaAySz3qF",
+    "githubkey" : "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"
+}
